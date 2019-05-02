6b665b92d3e52d9c29f8c3b1c52868ce9d4ba415
ygout <yohann.goutaret@gmail.com>
2019-05-02 22:40:37 +0200

    first commit

---
+++ apptwo/views.py
@@ -0,0 +1,31 @@
+from django.http import HttpResponse
+from django.template import loader
+from django.shortcuts import render
+
+
+# Create your views here.
+def djangorocks(request):
+    return HttpResponse("This is a Jazzy Response")
+
+
+def picture_detail(request, category, year=0, month=0, day=0):
+    template = loader.get_template('apptwo/index.html')
+
+    picture = {
+        'filename': 'mountain.jpg',
+        'categories': ['color', 'landscape' ,],
+    }
+
+    context = {
+        'page_title': "this is the picture detail",
+        'description': "<p>This <b>picture</b> was take on Mount Fuji</p>",
+        'category': category,
+        'year': year,
+        'month': month,
+        'day': day,
+        'picture': picture,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+
